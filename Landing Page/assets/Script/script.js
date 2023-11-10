63438c9018fd46062393891dfa7fa8c91d6d438f
Ezequiel <ezequielfilho404@gmail.com>
2023-11-10 05:24:44 -0300

    adding landig page files

---
+++ Landing Page/assets/Script/script.js
@@ -0,0 +1,27 @@
+const carouselContents = document.querySelectorAll('.carousel-content');
+
+let currentIndex = 0;
+
+function showContent(index) {
+    carouselContents.forEach((content, i) => {
+        if (i === index) {
+            content.classList.remove('inactive');
+            content.classList.add('active');
+        } else {
+            content.classList.remove('active');
+            content.classList.add('inactive');
+        }
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % carouselContents.length;
+    showContent(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + carouselContents.length) % carouselContents.length;
+    showContent(currentIndex);
+}
+
+setInterval(nextSlide, 10000);
