b4f0d25e2ae7f874d522f8492349e4e12c6d1411
Ezequiel Ferreira <105008901+EzArmy@users.noreply.github.com>
2024-04-02 20:17:08 -0300

    Alterçãoi back-end 02-04-24
    Alteração na edição de pastas do gestor

--- Projeto/vigillatus/private/routes/edit.js
+++ Projeto/vigillatus/private/routes/edit.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
 
 //Module import
 const Cargo = require('../module/Cargo');
@@ -11,66 +12,87 @@ const Gestor = require('../module/Gestor')
 /// CONFIGURAÇÕES /////////////////////////////////////////
 
 const gestorFotoStorage = multer.diskStorage({
-    destination:(req, file, cb)=>{
+    destination: (req, file, cb) => {
         const gestorFolder = './public/uploads/' + req.session.user.nome;
         cb(null, gestorFolder);
     },
-    filename:(req,file,cb)=>{
+    filename: (req, file, cb) => {
         const fileName = path.basename(file.originalname)
         cb(null, fileName);
     }
 });
 
 const editGestorUpload = multer({
-    storage:gestorFotoStorage
+    storage: gestorFotoStorage
 })
 
 /// ROTAS /////////////////////////////////////////
 
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res) => {
 
     const gestorInfo = req.session.user;
     const listaSetores = await Setor.findAll();
     const listaCargos = await Cargo.findAll({
-        where:{
+        where: {
             nivel: 1
         }
     });
 
-    res.render('editGestor.ejs',{
-        error:null,
-        listaSetores:listaSetores,
-        listaCargos:listaCargos,
-        gestorInfo:gestorInfo
+    res.render('editGestor.ejs', {
+        error: null,
+        listaSetores: listaSetores,
+        listaCargos: listaCargos,
+        gestorInfo: gestorInfo
     });
 });
 
-router.post('/confirmEdit', editGestorUpload.single('gestorFoto'), async (req, res)=>{
+router.post('/confirmEdit', editGestorUpload.single('gestorFoto'), async (req, res) => {
 
-    const{
+    const {
         gestorNome,
         gestorEmail,
         gestorSenha,
-        gestorSetor,
-        gestorCargo
+        idSetor: gestorSetor,
+        idCargo: gestorCargo
     } = req.body;
-
-    const gestorFoto = req.file.filename;
 
     const gestorInfo = req.session.user;
 
+    const gestorFoto = req.file ? req.file.filename : gestorInfo.foto;
+
+    //Variáveis para armazenar os nomes das pastas casdo tenha sido editada ou não
+    const oldGestorFolder = `./public/uploads/${gestorInfo.nome}`;
+    const newGestorFolder = `./public/uploads/${gestorNome}`;
+
+    //Verifica a alteração no nome do gestor
+
+    if(gestorInfo.nome == gestorNome){
+        console.log('Não é necessário alterar o nome da pasta do usuário ' + gestorInfo.nome);
+    }else{
+        fs.renameSync(oldGestorFolder, newGestorFolder);
+    }
+
     const editGestor = await Gestor.update({
-        nome:gestorNome,
-        email:gestorEmail,
-        senha:gestorSenha,
+        nome: gestorNome,
+        email: gestorEmail,
+        senha: gestorSenha,
         foto: gestorFoto,
         gestorSetor,
         gestorCargo
-    },{
-        where:{
+    }, {
+        where: {
             id: gestorInfo.id
         }
     });
+
+    // Atualiza os dados do gestor na sessão
+    req.session.user = {
+        ...req.session.user,
+        nome: gestorNome,
+        email: gestorEmail,
+        foto: gestorFoto
+        // Adicione outros campos da sessão, se necessário
+    };
 
     res.redirect('/home/perfilGestor');
 });
