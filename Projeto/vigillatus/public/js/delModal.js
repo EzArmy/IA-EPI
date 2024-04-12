5eee088ec7f8d90ce8a2433fd72f1c310fe3fe5f
Ezequiel Ferreira <105008901+EzArmy@users.noreply.github.com>
2024-04-11 22:57:46 -0300

    back/front-end update 11-04-24
    Modal de confimação para deletar o gestor foi criado com sucesso

---
+++ Projeto/vigillatus/public/js/delModal.js
@@ -0,0 +1,9 @@
+function showModal(){
+    var delModal = document.getElementById("delModal");
+    delModal.classList.toggle("show");
+}
+
+function closeModal(){
+    var delModal = document.getElementById("delModal");
+    delModal.classList.toggle("show")
+}
