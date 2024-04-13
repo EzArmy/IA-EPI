69d32b9e1bfb9c8a5edb0f5e05a600489270965f
Ezequiel Ferreira <105008901+EzArmy@users.noreply.github.com>
2024-04-12 21:43:19 -0300

    front-end update 12-04-24

--- Projeto/vigillatus/public/js/delModal.js
+++ Projeto/vigillatus/public/js/delModal.js
@@ -1,12 +1,49 @@
-function showModal(){
-    var delModal = document.getElementById("delModal");
-    delModal.classList.toggle("show");
+function randomGen(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function closeModal(){
-    var delModal = document.getElementById("delModal");
+function randomNums() {
+    var randomNum = document.getElementById('randomNum');
+    var num = [];
 
+    for (var i = 0; i < 6; i++) {
+        num.push(randomGen(1, 9));
+    }
+
+    randomNum.innerHTML = num.join('');
+}
+
+function showModal() {
+    var delModal = document.getElementById("delModal");
+    delModal.classList.toggle("show");
+
+    randomNums()
+}
+
+function closeModal() {
+    var delModal = document.getElementById("delModal");
     document.getElementById('confirmInput').value = '';
     delModal.classList.toggle("show");
-    
+
+}
+
+function numCheck() {
+    /* capturando os números apresentados para o gestor */
+    const randomNum = document.getElementById('randomNum').innerHTML;
+
+    /* capturando os números digitados pelo gestor no input */
+    var checkInput = document.getElementById('checkInput').value;
+
+    /* Botão de delete */
+    var delBtn = document.getElementById('delBtn');
+
+    if(checkInput == randomNum){
+        delBtn.setAttribute('disabled', 'false');
+        delBtn.style.backgroundColor = 'green';
+        console.log('São enguais *-*)');
+    }else{
+        delBtn.setAttribute('disabled', 'true');
+        delBtn.style.backgroundColor = 'red'
+        console.log('Não são enguais ._.)');
+    }
 }
